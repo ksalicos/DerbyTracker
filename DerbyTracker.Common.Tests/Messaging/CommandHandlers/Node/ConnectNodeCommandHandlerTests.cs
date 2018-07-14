d5e8c38f3aa28c07524b6fcb367db2f3ad915f64
Kevin <spam@madpoet.com>
2018-07-14 00:18:39 -0700

    Tests for ConnectNodeCommandHandler

---
+++ DerbyTracker.Common.Tests/Messaging/CommandHandlers/Node/ConnectNodeCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using DerbyTracker.Common.Messaging.CommandHandlers.Node;
+using DerbyTracker.Common.Messaging.Commands.Node;
+using DerbyTracker.Common.Messaging.Events.Node;
+using DerbyTracker.Common.Services;
+using System.Linq;
+using Xunit;
+
+namespace DerbyTracker.Common.Tests.Messaging.CommandHandlers.Node
+{
+    public class ConnectNodeCommandHandlerTests
+    {
+        private readonly INodeService _nodeService = new NodeService();
+
+        [Fact]
+        public void NewNodeCanConnect()
+        {
+            var handler = new ConnectNodeCommandHandler(_nodeService);
+            var command = new ConnectNodeCommand("NodeId", "ConnectionId");
+            var result = handler.Handle(command);
+            var @event = result.Events.FirstOrDefault(x => x.Event.type == "NODE_CONNECTED")?.Event as NodeConnectedEvent;
+            Assert.NotNull(@event);
+            Assert.Equal("NodeId", @event.NodeId);
+        }
+
+        [Fact]
+        public void NodeGetsSameNumberOnReconnect()
+        {
+            var handler = new ConnectNodeCommandHandler(_nodeService);
+            var command = new ConnectNodeCommand("NodeId", "ConnectionId");
+            var result = handler.Handle(command);
+            var @event = result.Events.FirstOrDefault(x => x.Event.type == "NODE_CONNECTED")?.Event as NodeConnectedEvent;
+            var result2 = handler.Handle(command);
+            var event2 = result2.Events.FirstOrDefault(x => x.Event.type == "NODE_CONNECTED")?.Event as NodeConnectedEvent;
+            Assert.NotNull(@event);
+            Assert.NotNull(event2);
+            Assert.Equal(@event.ConnectionNumber, event2.ConnectionNumber);
+        }
+    }
+}
